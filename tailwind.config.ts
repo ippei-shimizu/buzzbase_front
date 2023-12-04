2f07141f39926ed4dd3a40660d9793b2d205ae47
ippei-shimizu <ippei.shimizu.32@gmail.com>
2023-12-04 15:59:11 +0900

    ✨ Add: color追加

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,12 +26,22 @@ const config: Config = {
           800: "#62420E",
           900: "#312107",
         },
+        zic: {
+          50: "#FAFAFA",
+          100: "#F4F4F5",
+          200: "#E4E4E7",
+          300: "#D4D4D8",
+          400: "#A1A1AA",
+          500: "#71717A",
+          600: "#52525B",
+          700: "#3F3F46",
+          800: "#27272A",
+          900: "#18181B",
+        },
       },
     },
   },
   darkMode: "class",
-  plugins: [nextui(
-    
-  )],
+  plugins: [nextui()],
 };
 export default config;
