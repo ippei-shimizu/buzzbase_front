b0bf3963675941f20255b2aeb9a4d2ff13636f02
ippei-shimizu <ippei.shimizu.32@gmail.com>
2023-12-04 15:59:32 +0900

    🎨 Fix: style修正

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,10 +1,10 @@
 import { Button } from "@nextui-org/react";
-import { UserIcon } from "@app/components/modules/UserIcon";
+import { UserIcon } from "@app/components/icon/UserIcon";
 
 export default function Header() {
   return (
     <>
-      <header className="py-2 px-3">
+      <header className="py-2 px-3 border-b border-b-zinc-500">
         <div className="flex items-center justify-between h-full">
           <a href="">LOGO</a>
           <div className="flex items-center gap-x-4">
@@ -18,7 +18,7 @@ export default function Header() {
                   filled="#fff"
                   height="18"
                   width="18"
-                  label="User Icon"
+                  label=""
                 />
               }
               className="gap-x-1 bg-yellow-500 text-white text-sm py-1.5 px-3 h-auto rounded-lg"
