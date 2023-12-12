693ddeb55740c8e8e4022407f344722280263681
ippei-shimizu <ippei.shimizu.32@gmail.com>
2023-12-12 22:27:29 +0900

    🎨 Fix: 07 型定義を別ファイルで管理

--- app/services/authService.tsx
+++ app/services/authService.tsx
@@ -1,14 +1,4 @@
 import axiosInstance from "@app/utils/axiosInstance";
-interface SignUpData {
-  email: string;
-  password: string;
-  passwordConfirmation: string;
-}
-
-interface SignInData {
-  email: string;
-  password: string;
-}
 
 export const singUp = async (data: SignUpData) => {
   return axiosInstance.post("/api/v1/auth", {
