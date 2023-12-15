b07133d41557571bb2bf5dc412193686ed1bcb66
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2023-12-16 00:24:41 +0900

    💄 Fix: 16 UI修正

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -5,7 +5,7 @@ export default function page() {
   return (
     <div className="h-full flex flex-col items-center justify-center">
       <div className="bg-sub w-11/12 px-6 py-8 rounded-3xl">
-        <h2 className="text-xl font-bold mb-8">新規会員登録</h2>
+        <h2 className="text-2xl font-bold mb-8">新規会員登録</h2>
         <SignUp />
       </div>
     </div>
