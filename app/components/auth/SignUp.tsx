a8ca22b18ffc98aeccf8b1ea0a1bad75d5342660
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2023-12-17 17:15:01 +0900

    💄 Fix: エラーメッセージの表示位置を修正

--- app/components/auth/SignUp.tsx
+++ app/components/auth/SignUp.tsx
@@ -75,7 +75,7 @@ export default function SignUp() {
         className="flex flex-col justify-end gap-y-4"
       >
         {errors.length > 0 ? (
-          <ul className="w-10/12 fixed top-20 left-1/2 -translate-x-1/2 bg-danger-300 p-4 rounded-xl flex flex-col justify-center gap-y-1.5 z-10">
+          <ul className="w-10/12 fixed top-8 left-1/2 -translate-x-1/2 bg-danger-300 p-4 rounded-xl flex flex-col justify-center gap-y-1.5 z-10">
             {errors.map((error, index) => (
               <li key={index} className="text-xs flex items-center gap-x-2">
                 <DangerIcon
