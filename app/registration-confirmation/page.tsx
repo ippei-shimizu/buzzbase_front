9fca3c94c7c4d5f971aade0217511f37c683efb3
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2023-12-19 01:09:39 +0900

    🎨 Fix: 19 文言修正

--- app/registration-confirmation/page.tsx
+++ app/registration-confirmation/page.tsx
@@ -10,7 +10,7 @@ export default function RegistrationConfirmation() {
           BuzzBaseへご登録ありがとうございます！
         </h2>
         <p className=" text-base leading-6">
-          あなたのメールアドレス（user@example.com）にアカウント確認メールを送信しました。
+          入力したメールアドレス宛にアカウント確認メールを送信しました。
           <br></br>
           メール内のリンクをクリックして、登録を完了してください。メールが見つからない場合は、迷惑メールフォルダを確認するか、
           <Link href="/signup" className=" text-blue-400">
