f1cc7a7ef8678c5307fb84f70f5199b7db0107f2
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2023-12-27 20:34:40 +0900

    ⚡️ Add: 25 成績一覧絞り込みセレクトボックスUI実装

--- app/components/user/IndividualResultsList.tsx
+++ app/components/user/IndividualResultsList.tsx
@@ -1,3 +1,36 @@
+import ResultsSelectBox from "@app/components/select/ResultsSelectBox";
+import { gameType, years } from "@app/test/TestData";
+
 export default function IndividualResultsList() {
-  return <>成績</>;
+  return (
+    <>
+      <div className="bg-bg_sub p-4 rounded-xl">
+        <div className="flex gap-x-4 mb-4">
+          <ResultsSelectBox
+            radius="full"
+            defaultSelectedKeys={[years[0].label]}
+            className="bg-transparent rounded-full text-xs border-zinc-400 max-w-xs"
+            data={years}
+            variant="bordered"
+            color="primary"
+            ariaLabel="シーズンを選択"
+            labelPlacement="outside"
+            size="sm"
+          />
+          <ResultsSelectBox
+            radius="full"
+            defaultSelectedKeys={[gameType[0].label]}
+            className="bg-transparent rounded-full text-xs border-zinc-400 max-w-xs"
+            data={gameType}
+            variant="bordered"
+            color="primary"
+            ariaLabel="試合の種類を選択"
+            labelPlacement="outside"
+            size="sm"
+          />
+        </div>
+        <h2 className="text-lg">打撃成績</h2>
+      </div>
+    </>
+  );
 }
