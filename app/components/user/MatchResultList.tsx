4f71fbca479b17d3fa0393d6e9ee38f094c6eb69
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2023-12-28 00:10:01 +0900

    ✨ Add: 25 試合結果一覧コンポーネント作成

--- app/components/user/MatchResultList.tsx
+++ app/components/user/MatchResultList.tsx
@@ -1,7 +1,43 @@
+import MatchResultsItem from "@app/components/listItem/MatchResultsItem";
+import ResultsSelectBox from "@app/components/select/ResultsSelectBox";
+import { gameType, years } from "@app/test/TestData";
+
 export default function MatchResultList() {
   return (
     <>
-      <div>試合結果</div>
+      <div className="bg-bg_sub p-4 rounded-xl">
+        <div className="flex gap-x-4 mb-5">
+          <ResultsSelectBox
+            radius="full"
+            defaultSelectedKeys={[years[0].label]}
+            className="bg-transparent rounded-full text-xs border-zinc-400 max-w-xs"
+            data={years}
+            variant="bordered"
+            color="primary"
+            ariaLabel="シーズンを選択"
+            labelPlacement="outside"
+            size="sm"
+          />
+          <ResultsSelectBox
+            radius="full"
+            defaultSelectedKeys={[gameType[0].label]}
+            className="bg-transparent rounded-full text-xs border-zinc-400 max-w-xs"
+            data={gameType}
+            variant="bordered"
+            color="primary"
+            ariaLabel="試合の種類を選択"
+            labelPlacement="outside"
+            size="sm"
+          />
+        </div>
+        <div className="mt-8">
+          {Array.from({ length: 5 }, (_, index) => (
+            <div key={index} className="mt-8">
+              <MatchResultsItem />
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
