57f939a8fbdcf3bb72f99f6d0d4e6f00b0c330cb
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2023-12-29 23:50:50 +0900

    🩹 Fix: 28 成功トーストの表示判定条件追加

--- app/components/toast/ToastSuccess.tsx
+++ app/components/toast/ToastSuccess.tsx
@@ -3,10 +3,14 @@ import { CheckIcon } from "@app/components/icon/CheckIcon";
 export default function ToastSuccess({ text }: ToastSuccessProps) {
   return (
     <>
-      <div className="fixed top-14 right-4 flex items-center gap-x-2 px-4 py-2 bg-green-700 rounded-xl z-10">
-        <CheckIcon width="16" height="16" fill="#fff" />
-        <p className="text-sm">{text}</p>
-      </div>
+      {text.length > 0 ? (
+        <div className="fixed top-14 right-4 flex items-center gap-x-2 px-4 py-2 bg-green-700 rounded-xl z-10">
+          <CheckIcon width="16" height="16" fill="#fff" />
+          <p className="text-sm">{text}</p>
+        </div>
+      ) : (
+        ""
+      )}
     </>
   );
 }
