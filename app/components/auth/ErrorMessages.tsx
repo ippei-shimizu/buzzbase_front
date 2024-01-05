044422d4f29597f12db6f52c9cc2c7844d64f0ad
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2024-01-06 04:32:57 +0900

    🐛 Fix: エラーメッセージnull許容

--- app/components/auth/ErrorMessages.tsx
+++ app/components/auth/ErrorMessages.tsx
@@ -3,7 +3,7 @@ import { DangerIcon } from "@app/components/icon/DangerIcon";
 export default function ErrorMessages({ errors }: ErrorMessagesProps) {
   return (
     <>
-      {errors.length > 0 ? (
+      {errors?.length > 0 ? (
         <ul className="w-10/12 fixed top-16 left-1/2 -translate-x-1/2 bg-danger-300 p-4 rounded-xl flex flex-col justify-center gap-y-1.5 z-50">
           {errors.map((error, index) => (
             <li key={index} className="text-xs flex items-center gap-x-2">
