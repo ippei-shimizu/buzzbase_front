575d00a1ea36f7bceb1de3e85ad8b32c3682dbdc
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2024-01-19 01:45:18 +0900

    ✨ Add: 30 matchResult API

---
+++ app/services/matchResultsService.tsx
@@ -0,0 +1,62 @@
+import axiosInstance from "@app/utils/axiosInstance";
+
+export const getMatchResults = async () => {
+  try {
+    const response = await axiosInstance.get("/api/v1/match_results");
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const createMatchResults = async (
+  matchResultsData: MatchResultsData
+) => {
+  try {
+    const response = await axiosInstance.post(
+      "/api/v1/match_results",
+      matchResultsData
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const getMatchResult = async (id: number) => {
+  try {
+    const response = await axiosInstance.get(`/api/v1/match_results/${id}`);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const updateMatchResult = async (
+  id: number,
+  matchResultsData: MatchResultsData
+) => {
+  try {
+    const response = await axiosInstance.put(
+      `/api/v1/match_results/${id}`,
+      matchResultsData
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const deleteMatchResult = async (id: number) => {
+  try {
+    const response = await axiosInstance.delete(`/api/v1/match_results/${id}`);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
