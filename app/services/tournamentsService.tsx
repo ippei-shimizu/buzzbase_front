406149f49b4b3965f9300ff331b7cc6ea08595c1
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2024-01-19 01:44:49 +0900

    ✨ Add: 30 tournamentsAPI

---
+++ app/services/tournamentsService.tsx
@@ -0,0 +1,24 @@
+import axiosInstance from "@app/utils/axiosInstance";
+
+export const getTournaments = async () => {
+  try {
+    const response = await axiosInstance.get("/api/v1/tournaments");
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const createTournament = async (tournamentData: { name: string }) => {
+  try {
+    const response = await axiosInstance.post(
+      "/api/v1/tournaments",
+      tournamentData
+    );
+    return response.data as TournamentData;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
