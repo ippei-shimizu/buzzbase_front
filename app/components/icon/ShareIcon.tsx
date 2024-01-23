67a20a50ee86a58afec4181410ffbe0191c90808
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2024-01-23 20:55:39 +0900

    ✨ Add: 33 シェアアイコンコンポーネント

---
+++ app/components/icon/ShareIcon.tsx
@@ -0,0 +1,38 @@
+export const ShareIcon = ({
+  width = "18",
+  height = "18",
+  fill = "none",
+  stroke = "",
+}) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={height}
+      viewBox="0 0 18 18"
+      fill={fill}
+    >
+      <path
+        d="M12.33 6.6748C15.03 6.9073 16.1325 8.2948 16.1325 11.3323V11.4298C16.1325 14.7823 14.79 16.1248 11.4375 16.1248H6.55499C3.20249 16.1248 1.85999 14.7823 1.85999 11.4298V11.3323C1.85999 8.3173 2.94749 6.9298 5.60249 6.6823"
+        stroke={stroke}
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M9 11.2498V2.71484"
+        stroke={stroke}
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M11.5125 4.3875L9.00005 1.875L6.48755 4.3875"
+        stroke={stroke}
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
