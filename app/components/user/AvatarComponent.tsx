55e8f0245c9aac59bbdd72dd4b292ec7aa6a8247
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2024-01-27 06:17:17 +0900

    ⚡️ Fix: アバターコンポーネント化

---
+++ app/components/user/AvatarComponent.tsx
@@ -0,0 +1,29 @@
+import { Avatar } from "@nextui-org/react";
+
+type UserData = {
+  userData: {
+    image: any;
+    name: string;
+    user_id: string;
+    url: string;
+  };
+};
+
+export default function AvatarComponent(props: UserData) {
+  const { userData } = props;
+  return (
+    <div className="flex gap-5">
+      <Avatar
+        size="lg"
+        isBordered
+        src={`${process.env.NEXT_PUBLIC_API_URL}${userData.image.url}`}
+      />
+      <div className="flex flex-col gap-1.5 items-start justify-center">
+        <h1 className="text-lg font-semibold leading-none">{userData.name}</h1>
+        <p className="text-sm tracking-tight text-zinc-400">
+          @{userData.user_id}
+        </p>
+      </div>
+    </div>
+  );
+}
--- app/mypage/[slug]/page.tsx
+++ app/mypage/[slug]/page.tsx
@@ -7,13 +7,14 @@ import LoadingSpinner from "@app/components/spinner/LoadingSpinner";
 import IndividualResultsList from "@app/components/user/IndividualResultsList";
 import MatchResultList from "@app/components/user/MatchResultList";
 import { getUserData } from "@app/services/userService";
-import { Avatar, Button, Link, Tab, Tabs } from "@nextui-org/react";
+import { Button, Link, Tab, Tabs } from "@nextui-org/react";
 import React, { useEffect, useState } from "react";
 import Header from "@app/components/header/Header";
 import { getTeams } from "@app/services/teamsService";
 import { getBaseballCategory } from "@app/services/baseballCategoryService";
 import { getPrefectures } from "@app/services/prefectureService";
 import { getUserAwards } from "@app/services/awardsService";
+import AvatarComponent from "@app/components/user/AvatarComponent";
 
 type Position = {
   id: string;
@@ -100,21 +101,7 @@ export default function MyPage() {
         <main className="h-full">
           <div className="pt-16 pb-36 bg-main">
             <div className=" px-4">
-              <div className="flex gap-5">
-                <Avatar
-                  size="lg"
-                  isBordered
-                  src={`${process.env.NEXT_PUBLIC_API_URL}${userData.image.url}`}
-                />
-                <div className="flex flex-col gap-1.5 items-start justify-center">
-                  <h1 className="text-lg font-semibold leading-none">
-                    {userData.name}
-                  </h1>
-                  <p className="text-sm tracking-tight text-zinc-400">
-                    @{userData.user_id}
-                  </p>
-                </div>
-              </div>
+              <AvatarComponent userData={userData} />
               {userData.introduction?.length > 0 ? (
                 <>
                   <p className="text-sm mt-4">{userData.introduction}</p>
