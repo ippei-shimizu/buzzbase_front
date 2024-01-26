f3a3f2b6186e3f2481253f4a447070993ef67423
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2024-01-27 02:03:04 +0900

    ✨ Add: 72 試合データ取得リクエスト

--- app/services/gameResultsService.tsx
+++ app/services/gameResultsService.tsx
@@ -2,7 +2,9 @@ import axiosInstance from "@app/utils/axiosInstance";
 
 export const getGameResults = async () => {
   try {
-    const response = await axiosInstance.get("/api/v1/game_result");
+    const response = await axiosInstance.get(
+      "/api/v1/game_results/game_associated_data_index"
+    );
     return response.data;
   } catch (error) {
     console.log(error);
