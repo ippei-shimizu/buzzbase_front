9c375be1bf78f153f80c9335d08ed40ac7b7f39b
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2024-01-27 00:20:46 +0900

    ✨ Add: 打撃・投手id作成リクエスト

--- app/services/gameResultsService.tsx
+++ app/services/gameResultsService.tsx
@@ -35,3 +35,35 @@ export const updateGameResult = async (
     throw error;
   }
 };
+
+export const updateBattingAverageId = async (
+  id: number,
+  data: { game_result: { batting_average_id: number } }
+) => {
+  try {
+    const response = await axiosInstance.put(
+      `/api/v1/game_results/${id}/update_batting_average_id`,
+      data
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const updatePitchingResultId = async (
+  id: number,
+  data: { game_result: { pitching_result_id: number } }
+) => {
+  try {
+    const response = await axiosInstance.put(
+      `/api/v1/game_results/${id}/update_pitching_result_id`,
+      data
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
