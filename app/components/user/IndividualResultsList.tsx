1a869d735786fa778cb5770246552622901f3f85
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2024-01-28 04:11:34 +0900

    ✨ Add: 37 個人打撃成績（通算）表示

--- app/components/user/IndividualResultsList.tsx
+++ app/components/user/IndividualResultsList.tsx
@@ -2,13 +2,31 @@ import ResultsSelectBox from "@app/components/select/ResultsSelectBox";
 import BattingAverageTable from "@app/components/table/BattingAverageTable";
 import PitchingRecordTable from "@app/components/table/PitchingRecordTable";
 import { gameType, years } from "@app/data/TestData";
+import { getPersonalBattingAverage } from "@app/services/battingAveragesService";
+import { useEffect, useState } from "react";
 
 export default function IndividualResultsList() {
+  const [personalBattingAverages, setPersonalBattingAverages] = useState([]);
+
+  useEffect(() => {
+    fetchBattingAverages();
+  }, []);
+
+  const fetchBattingAverages = async () => {
+    try {
+      const response = await getPersonalBattingAverage();
+      setPersonalBattingAverages(response);
+    } catch (error) {
+      console.log(`Fetch Personal Batting Averages error:`, error);
+    }
+  };
+
+  console.log(personalBattingAverages);
   return (
     <>
       <div className="bg-bg_sub p-4 rounded-xl">
         <div className="flex gap-x-4 mb-5">
-          <ResultsSelectBox
+          {/* <ResultsSelectBox
             radius="full"
             defaultSelectedKeys={[years[0].label]}
             className="bg-transparent rounded-full text-xs border-zinc-400 max-w-xs"
@@ -29,10 +47,12 @@ export default function IndividualResultsList() {
             ariaLabel="試合の種類を選択"
             labelPlacement="outside"
             size="sm"
-          />
+          /> */}
         </div>
         <h2 className="text-xl">打撃成績</h2>
-        <BattingAverageTable />
+        <BattingAverageTable
+          personalBattingAverages={personalBattingAverages}
+        />
         <h2 className="text-xl mt-8">投手成績</h2>
         <PitchingRecordTable />
       </div>
