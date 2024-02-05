59e9f7be1096262ace74059d9934b8e88c784ade
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2024-02-06 00:53:50 +0900

    ✨ Add: フォローユーザー招待

--- app/groups/new/page.tsx
+++ app/groups/new/page.tsx
@@ -1,16 +1,40 @@
 "use client";
 import HeaderMatchResultNext from "@app/components/header/HeaderMatchResultSave";
 import { createGroup } from "@app/services/groupService";
-import { Avatar, Input } from "@nextui-org/react";
-import { useRef, useState } from "react";
+import { getCurrentUserId, getFollowingUser } from "@app/services/userService";
+import { Avatar, Checkbox, Input, User } from "@nextui-org/react";
+import { useEffect, useRef, useState } from "react";
 
 export default function GroupNew() {
+  const [currentUserId, setCurrentUserId] = useState(null);
+  const [following, setFollowing] = useState<FollowingUser[]>([]);
+  const [selectedUserIds, setSelectedUserIds] = useState<number[]>([]);
   const [group, setGroup] = useState<{ name: string; icon: string | null }>({
     name: "",
     icon: null,
   });
-
   const fileInputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    fetchData();
+    if (currentUserId) {
+      fetchFollowingUser(currentUserId);
+    }
+  }, [currentUserId]);
+
+  const fetchData = async () => {
+    const responseCurrentUserId = await getCurrentUserId();
+    setCurrentUserId(responseCurrentUserId);
+  };
+
+  const fetchFollowingUser = async (userId: number) => {
+    try {
+      const response = await getFollowingUser(userId);
+      setFollowing(response);
+    } catch (error) {
+      console.log("フォロー中のユーザーはいません", error);
+    }
+  };
 
   const handleImageClick = () => {
     fileInputRef.current?.click();
@@ -18,14 +42,14 @@ export default function GroupNew() {
 
   const handleChange = (event: any) => {
     if (event.target.name === "image") {
-      const previewFileUrl =
-        event.target.files && event.target.files[0]
-          ? URL.createObjectURL(event.target.files[0])
-          : null;
-      setGroup((prevState) => ({
-        ...prevState,
-        icon: previewFileUrl,
-      }));
+      const file = event.target.files[0];
+      if (file) {
+        const previewFileUrl = URL.createObjectURL(file);
+        setGroup((prevState) => ({
+          ...prevState,
+          icon: previewFileUrl,
+        }));
+      }
     } else {
       setGroup((prevState) => ({
         ...prevState,
@@ -34,13 +58,26 @@ export default function GroupNew() {
     }
   };
 
+  const handleCheckboxChange = (userId: number, isChecked: boolean) => {
+    setSelectedUserIds((prevSelectedUserIds) => {
+      if (isChecked) {
+        return [...prevSelectedUserIds, userId];
+      } else {
+        return prevSelectedUserIds.filter((id) => id !== userId);
+      }
+    });
+  };
+
   const handleSubmit = async (event: any) => {
     event.preventDefault();
     const formData = new FormData();
     formData.append("group[name]", group.name);
-    if (group.icon && typeof group.icon === "string") {
-      formData.append("group[icon]", group.icon);
+    if (fileInputRef.current?.files && fileInputRef.current.files.length > 0) {
+      formData.append("group[icon]", fileInputRef.current.files[0]);
     }
+    selectedUserIds.forEach((userId) => {
+      formData.append("invite_user_ids[]", userId.toString());
+    });
     try {
       await createGroup(formData);
       console.log(group);
@@ -49,14 +86,16 @@ export default function GroupNew() {
     }
   };
 
+  console.log(selectedUserIds);
+
   return (
     <>
       <div className="buzz-dark">
         <HeaderMatchResultNext onMatchResultNext={handleSubmit} text={"作成"} />
-        <div className="h-full">
+        <div className="h-full bg-main">
           <main className="h-full">
             <div className="px-4 py-14">
-              <h2 className="text-xl font-bold mt-5">グループ設定</h2>
+              <h2 className="text-2xl font-bold mt-5">グループ設定</h2>
               <form>
                 <div className="grid grid-cols-[72px_1fr] gap-x-6 items-start mt-6">
                   <div className="flex justify-center flex-col items-center">
@@ -95,6 +134,30 @@ export default function GroupNew() {
                       onChange={handleChange}
                     />
                   </div>
+                </div>
+                <p className="pt-12 text-lg font-bold">メンバーを選択</p>
+                <div className="pt-4 pb-24 grid gap-y-5 bg-main">
+                  {following.map((follow) => (
+                    <div
+                      key={follow.id}
+                      className="flex justify-between items-center"
+                    >
+                      <User
+                        name={follow.name}
+                        description={follow.user_id}
+                        avatarProps={{
+                          src: `${process.env.NEXT_PUBLIC_API_URL}${follow.image.url}`,
+                        }}
+                      />
+                      <Checkbox
+                        size="lg"
+                        className="p-0"
+                        onChange={(e) =>
+                          handleCheckboxChange(follow.id, e.target.checked)
+                        }
+                      ></Checkbox>
+                    </div>
+                  ))}
                 </div>
               </form>
             </div>
