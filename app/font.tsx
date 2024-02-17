c897538d79acfe75d22f465fdea96e84881f85c8
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2024-02-17 20:09:37 +0900

    🐛 Fix: デプロイエラー

--- app/font.tsx
+++ app/font.tsx
@@ -1,4 +1,4 @@
-import { Inter, Noto_Sans_JP } from "next/font/google";
+import { Noto_Sans_JP } from "next/font/google";
 
 export const notoSansJP = Noto_Sans_JP({
   weight: ["300", "400", "500", "600"],
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,13 @@ import "./globals.css";
 import { Providers } from "@app/providers";
 import NavigationMenu from "@app/components/header/NavigationMenu";
 import { AuthProvider } from "@app/contexts/useAuthContext";
-import { inter, notoSansJP } from "@app/font";
+import { notoSansJP } from "@app/font";
 import Footer from "@app/components/footer/Footer";
 
 export const metadata: Metadata = {
   title: "BUZZ BASE - 野球の個人記録をランキング形式で共有",
-  description: "野球の個人成績を記録して、チーム内外の友達とランキング形式で成績を共有することができるアプリです。",
+  description:
+    "野球の個人成績を記録して、チーム内外の友達とランキング形式で成績を共有することができるアプリです。",
 };
 
 export const viewport: Viewport = {
@@ -25,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="ja" className="h-full">
       <body
-        className={`${inter.className} ${notoSansJP.className} bg-main text-white h-full`}
+        className={`${notoSansJP.className} bg-main text-white h-full`}
       >
         <AuthProvider>
           <Providers>
