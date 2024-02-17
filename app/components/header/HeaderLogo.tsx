76232ffebbdf5318450fd3fb98531a5fc65c43bf
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2024-02-17 21:43:32 +0900

    🐛 Fix: ロゴ設定

--- app/components/header/HeaderLogo.tsx
+++ app/components/header/HeaderLogo.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useEffect } from "react";
 import { Link } from "@nextui-org/react";
+import Image from "next/image";
 
 export default function HeaderLogo() {
   useEffect(() => {
@@ -10,7 +11,12 @@ export default function HeaderLogo() {
     <>
       <header className="py-2 px-3 border-b border-b-zinc-500 fixed top-0 w-full bg-main z-50">
         <div className="flex items-center justify-center h-full max-w-[692px] mx-auto lg:m-[0_auto_0_28%]">
-          <Link href="/">LOGO</Link>
+          <Image
+            src="/images/buzz-logo-v2.png"
+            width="120"
+            height="30"
+            alt="BUZZ BASE"
+          />
         </div>
       </header>
     </>
