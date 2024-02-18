ade343cd48124f160e5d9c8d4eab8c66e37ae476
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2024-02-18 19:25:59 +0900

    💄 Fix: @マーク

--- app/everyone/page.tsx
+++ app/everyone/page.tsx
@@ -55,7 +55,7 @@ export default function EveryoneGameResultList() {
                       >
                         <User
                           name={gameResult.user_name}
-                          description={gameResult.user_user_id}
+                          description={`@${gameResult.user_user_id}`}
                           avatarProps={{
                             src: `${process.env.NEXT_PUBLIC_API_URL}${gameResult.user_image.url}`,
                           }}
