f96c6b111c78aba37efdd9c07f3df11dc8d5c32a
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2024-02-20 02:20:06 +0900

    🐛 Fix: 画像が取得できない

--- app/components/user/AvatarComponent.tsx
+++ app/components/user/AvatarComponent.tsx
@@ -13,15 +13,14 @@ type UserData = {
 
 export default function AvatarComponent(props: UserData) {
   const { userData } = props;
+  const imageUrl =
+    process.env.NODE_ENV === "production"
+      ? userData.user.image.url
+      : `${process.env.NEXT_PUBLIC_S3_API_URL}${userData.user.image.url}`;
+
   return (
     <div className="flex gap-5">
-      <Avatar
-        size="lg"
-        isBordered
-        src={`${process.env.NEXT_PUBLIC_S3_API_URL || ""}${
-          userData.user.image.url
-        }`}
-      />
+      <Avatar size="lg" isBordered src={imageUrl} />
       <div className="flex flex-col gap-1.5 items-start justify-center">
         <h1 className="text-lg font-semibold leading-none">
           {userData.user.name}
