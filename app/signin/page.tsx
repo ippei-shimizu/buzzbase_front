11355312ab1d499c5153a9740ac8bdebb9166c0b
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2024-03-09 22:06:23 +0900

    💄 Fix: デザイン変更

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -2,6 +2,7 @@
 import SignIn from "@app/components/auth/SignIn";
 import ToastSuccess from "@app/components/toast/ToastSuccess";
 import { useAuthContext } from "@app/contexts/useAuthContext";
+import Image from "next/image";
 import { useSearchParams } from "next/navigation";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
@@ -40,6 +41,13 @@ export default function Page() {
   return (
     <>
       {logoutSuccess && <ToastSuccess text={message} />}
+      <Image
+        src="/images/logo-bg.png"
+        alt=""
+        width={500}
+        height={400}
+        className="absolute opacity-[0.03] -left-28 -top-10 lg:left-24 lg:w-[620px]"
+      />
       <div className="h-full flex flex-col items-center justify-center px-4">
         <div className="w-11/12 max-w-[720px] mx-auto lg:m-[0_auto_0_28%]">
           {confirmationUrl && (
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,13 +1,23 @@
 import SignUp from "@app/components/auth/SignUp";
+import Image from "next/image";
 import React from "react";
 
 export default function Page() {
   return (
-    <div className="h-full flex flex-col items-center justify-center">
-      <div className="w-11/12 px-4 max-w-[720px] mx-auto lg:m-[0_auto_0_28%]">
-        <h2 className="text-2xl font-bold mb-8">新規会員登録</h2>
-        <SignUp />
+    <>
+      <Image
+        src="/images/logo-bg.png"
+        alt=""
+        width={500}
+        height={400}
+        className="absolute opacity-[0.03] -left-28 -top-10 lg:left-24 lg:w-[620px]"
+      />
+      <div className="h-full flex flex-col items-center justify-center">
+        <div className="w-11/12 px-4 max-w-[720px] mx-auto lg:m-[0_auto_0_28%]">
+          <h2 className="text-2xl font-bold mb-8">新規会員登録</h2>
+          <SignUp />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
